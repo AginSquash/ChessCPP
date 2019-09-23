5477b588760e3f8ad98fe909f0292c2c6227bfb9
AginS <agins.main@gmail.com>
2019-09-23 19:13:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,23 +8,26 @@ int main() {
     sf::Texture texture;
     if (!texture.loadFromFile("resources/horse.jpg"))
         return EXIT_FAILURE;
+
     sf::Sprite sprite(texture);
 
     while (window.isOpen())
     {
         sf::Event event;
-        if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event))
         {
-            window.close();
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+
+            // Some CallBack functions will be here
         }
 
-        // Some CallBack functions will be here
-
+        window.clear();
+        window.draw( sprite );
+        window.display();
     }
-
-    window.clear();
-    window.draw( sprite );
-    window.display();
 
     return EXIT_SUCCESS;
 }
