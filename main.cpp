5b94f3db431422a41cb7277a2a6c2d6c9dd5c11f
AginS <agins.main@gmail.com>
2019-09-23 19:10:09 +0300

    Added resources folder & fix .gitignore

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@
 int main() {
 
     sf::RenderWindow window ( sf::VideoMode(900, 900), "ChessCPP");
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("resources/horse.jpg"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        if (event.type == sf::Event::Closed)
+        {
+            window.close();
+        }
+
+        // Some CallBack functions will be here
+
+    }
+
+    window.clear();
+    window.draw( sprite );
+    window.display();
+
+    return EXIT_SUCCESS;
 }
