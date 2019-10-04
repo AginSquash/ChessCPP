12752f94e7c684fee004e197e6537f4b0c5c8d8d
AginS <agins.main@gmail.com>
2019-10-04 11:28:24 +0300

    Update installer.py

--- installer/_macOS/installer.py
+++ installer/_macOS/installer.py
@@ -8,4 +8,4 @@ print(login)
 data_path = "/Users/" + login + "/.projects/" + appName + "/resources"
 shutil.copytree( "resources/", data_path)
 
-print("All data succefull instaled into " + data_path + "\nByr-bye.")
+print("All data succefull instaled into " + data_path + "\nNow you can run ChessCPP (just double-click on it).")
